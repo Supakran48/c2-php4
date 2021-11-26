7cd6e9a2ec8726bf141f83faad36afe36c600622
Supakran48 <flukeark1493@gmail.com>
2021-11-26 10:02:58 +0700

    update

--- func1.php
+++ func1.php
@@ -1,2 +1,39 @@
 <?php
 //Function
+
+function add(){
+    $a = 10;
+    $b = 5;
+    $add = $a + $b;
+    echo "This is a Function = ". $add;
+    echo "<br />";
+}
+function sud(){
+    $a = 10;
+    $b = 5;
+    $sud = $a - $b;
+    echo "This is a Function = ". $sud;
+    echo "<br />";
+}
+function division(){
+    $a = 10;
+    $b = 5;
+    $division = $a/$b;
+    echo "This is a Function = ". $division ;
+    echo "<br />";
+}
+function Multip(){
+    $a = 10;
+    $b = 5;
+    $Multip = $a * $b;
+    echo "This is a Function = ". $Multip ;
+    echo "<br />";
+}
+add();
+sud();
+division();
+Multip();
+
+
+
+
